1e212a114d85948fb143f920de18880aba89d940
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-20 22:24:42 +0100

    Add Repository Directory and implements RepositoriesInterface file, add interface methods

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Http\Interfaces\RepositoriesInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class Repository implements RepositoriesInterface
+{
+
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create([$data]);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $record = $this->model->find($id);
+        return $record->update([$data]);
+    }
+
+    public function destroy(int $id)
+    {
+        $record = $this->model->find($id);
+        return $record->delete();
+    }
+}
+

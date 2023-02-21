a5a9fa2a96f2609addd4b56a2213558c1562b70e
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-21 10:38:30 +0100

    Add bootstrap CDN

--- resources/views/Product/productIndex.blade.php
+++ resources/views/Product/productIndex.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Products</title>
 </head>
 <body>
     Hello world!
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>

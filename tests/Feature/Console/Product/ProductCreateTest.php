266dd8ef9290e10fa6ba03eaabb464f5f7b5f608
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-21 09:19:42 +0100

    Add Console/Product/ProductCreateTest derictory

---
+++ tests/Feature/Console/Product/ProductCreateTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Console\Product;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductCreateTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}

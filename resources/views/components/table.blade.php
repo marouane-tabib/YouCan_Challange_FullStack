c8453bbf673f91253770d449ef71b3446604ddda
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-21 12:02:31 +0100

    Add img directory in resources and table component to show all products

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,24 @@
+<table class="table table-dark">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Image</th>
+        <th scope="col">Name</th>
+        <th scope="col">Description</th>
+        <th scope="col">Price</th>
+        <th scope="col">Category</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($products as $product)
+            <tr>
+                <th scope="row">1</th>
+                <td><img height="55" width="80" src="{{ asset('storage/'.$product->image) }}" alt=""></td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->description }}</td>
+                <td>{{ $product->price }}</td>
+                <td>{{ $product->category->name }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+  </table>

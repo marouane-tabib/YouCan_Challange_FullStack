12e3daa48075b7d77e45be11e695c899468fe73e
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-21 11:08:39 +0100

    include modale component in productIndex.blade.php file

--- resources/views/Product/productIndex.blade.php
+++ resources/views/Product/productIndex.blade.php
@@ -8,8 +8,9 @@
     <title>Products</title>
 </head>
 <body>
+    @include('components.modal')
     Hello world!
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>

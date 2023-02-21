1b419ca716462154b573b6b5dc01c0c594cc79fd
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-21 11:37:27 +0100

    Add Send category data with index method return

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -2,18 +2,21 @@
 
 namespace App\Repositories;
 
+use App\Models\Category;
 use App\Models\Product;
 
 class ProductRepository extends Repository
 {
+    protected $category;
 
     public function __construct()
     {
         parent::__construct(new Product());
+        $this->category = new CategoryRepository();
     }
 
     public function index(){
-        return view('product.productIndex' , ['products' => $this->all()]);
+        return view('product.productIndex' , ['products' => $this->all() , 'categories' => $this->category->all()]);
     }
 
 }

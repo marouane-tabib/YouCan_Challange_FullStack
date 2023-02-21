2a872c301a44e5ce3c39eb280912d97a2cb8a1b7
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-21 09:28:42 +0100

    Add Product/ProductCreate Console Commands

---
+++ app/Console/Commands/Product/ProductCreate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\Product;
+
+use Illuminate\Console\Command;
+
+class ProductCreate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}

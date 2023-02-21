08b23ed8432d579ba841123bcd8682dc0d2c8c1e
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-21 10:34:01 +0100

    Add index repository return , and work-out to create method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    protected $repositoty;
+
+    public function __construct()
+    {
+        $this->repositoty = new ProductRepository;
+    }
+
+    public function index(){
+        return $this->repositoty->index();
+    }
+
+    public function create($id){
+        // 
+    }
 }

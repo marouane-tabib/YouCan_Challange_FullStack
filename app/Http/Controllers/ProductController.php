c593013bd865fcab3fad1bf0a1ca1b31fd74aa4a
marwan-tabib <marwanmaroc297@gmail.com>
2023-03-17 22:27:47 +0100

    Add index method - completed & inject the ProductRepositoryInterface - completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,30 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Interfaces\ProductRepositoryInterface;
 use App\Http\Requests\ProductRequest;
+use App\Models\Category;
 use App\Traits\ImageUploaderTrait;
-use App\Repositories\ProductRepository;
 
 class ProductController extends Controller
 {
     use ImageUploaderTrait;
 
-    protected $repository;
+    protected ProductRepositoryInterface $productRepository;
 
-    public function __construct()
+    public function __construct(ProductRepositoryInterface $productRepository)
     {
-        $this->repository = new ProductRepository;
+        $this->productRepository = $productRepository;
     }
 
     public function index(ProductRequest $request)
     {
-        return $this->repository->index($request->all());
+         $product = $request->filter ? $this->productRepository->filter($request->toArray()) : $this->productRepository->all();
+        $categories = Category::all();
+        return view('product.index', ['products' => $product, 'categories' => $categories]);
     }
 
     public function create(ProductRequest $request)
     {
-        $request = $request->validated();
-        $request['image'] = $this->uploadImage($request['image']);
-        return $this->repository->create($request);
+        // $request = $request->validated();
+        // $request['image'] = $this->uploadImage($request['image']);
+        // return $this->repository->create($request);
     }
 }

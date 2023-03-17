f9377331fc218b51592c8cb0f524536e3c00e954
marwan-tabib <marwanmaroc297@gmail.com>
2023-03-17 16:47:00 +0100

    implement the CategoryRepositoryInterface in the Repositories\CategoryRepository

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -2,14 +2,14 @@
 
 namespace App\Repositories;
 
+use App\Http\Interfaces\CategoryRepositoryInterface;
 use App\Models\Category;
 
-class CategoryRepository extends Repository
+class CategoryRepository implements CategoryRepositoryInterface
 {
 
-    public function __construct()
-    {
-        parent::__construct(new Category());
+    public function all(array $select = []){
+        return Category::all($select);
     }
 
 }

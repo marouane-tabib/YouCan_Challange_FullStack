4c95a46bc67483d9a9a1719cdc9976f5089d37f6
marwan-tabib <marwanmaroc297@gmail.com>
2023-03-18 21:36:42 +0100

    inject Product Model in the ProductRepository.php file

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -9,19 +9,24 @@ use Illuminate\Http\Client\Request;
 
 class ProductRepository implements ProductRepositoryInterface
 {
+    protected Product $product;
 
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
     public function all(){
-        return Product::all();
+        return $this->product::all();
     }
 
     public function create(array $data){
-        Product::create($data);
+        $this->product::create($data);
         return redirect()->back();
     }
 
     public function filter(array $filter)
     {
-        return Product::when($filter['category_filter'] != '' , function($query) use ($filter){
+        return $this->product::when($filter['category_filter'] != '' , function($query) use ($filter){
                         $query->where('category_id', $filter['category_filter']);
                     })
                     ->orderBy($filter['sort_by'] ?? 'id' , $filter['order_by'] ?? 'desc')

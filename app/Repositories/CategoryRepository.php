6764ea675d9ec9fa7f4bc93d3eb6f0e67fc021ec
marwan-tabib <marwanmaroc297@gmail.com>
2023-03-18 21:39:33 +0100

    inject Category Model in the CategoryRepository.php file

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -8,8 +8,15 @@ use App\Models\Category;
 class CategoryRepository implements CategoryRepositoryInterface
 {
 
+    protected Category $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
     public function all(array $select = []){
-        return Category::all($select);
+        return $this->category::all($select);
     }
 
 }

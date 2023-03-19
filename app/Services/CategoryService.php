6273898bb2343c3e1bb3397b120f91c8f874eb39
marwan-tabib <marwanmaroc297@gmail.com>
2023-03-19 16:24:30 +0100

    Add the CategoryService container methods , select all categories

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,7 +2,20 @@
 
 namespace App\Services;
 
+use App\Http\Interfaces\CategoryRepositoryInterface;
+
 class CategoryService
 {
-    // 
+
+    protected CategoryRepositoryInterface $categoryRepository;
+
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function all(array $select = ['*'])
+    {
+        return $this->categoryRepository->all($select);
+    }
 }

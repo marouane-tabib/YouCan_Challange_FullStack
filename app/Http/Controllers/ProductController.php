f87300b1c8749382a137faf2ad8531dc9ac830c5
marwan-tabib <marwanmaroc297@gmail.com>
2023-03-19 16:28:20 +0100

    clean the ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,20 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Interfaces\CategoryRepositoryInterface;
-use App\Http\Interfaces\ProductRepositoryInterface;
 use App\Http\Requests\ProductRequest;
-use App\Models\Category;
 use App\Services\CategoryService;
 use App\Services\ProductService;
-use App\Traits\ImageUploaderTrait;
 
 class ProductController extends Controller
 {
-    // use ImageUploaderTrait;
 
-    // protected ProductRepositoryInterface $productRepository;
-    // protected CategoryRepositoryInterface $categoryRepository;
     protected ProductService $productService;
     protected CategoryService $categoryService;
 
@@ -25,15 +18,8 @@ class ProductController extends Controller
         $this->categoryService = $categoryService;
     }
 
-    // public function __construct(ProductRepositoryInterface $productRepository, CategoryRepositoryInterface $categoryRepository)
-    // {
-    //     $this->productRepository = $productRepository;
-    //     $this->categoryRepository = $categoryRepository;
-    // }
-
     public function index(ProductRequest $request)
     {
-        // $product = $request->filter ? $this->productRepository->filter($request->toArray()) : $this->productRepository->all();
         $product = $this->productService->all($request);
         $categories = $this->categoryService->all(['id', 'name']);
         return view('product.index', ['products' => $product, 'categories' => $categories]);
